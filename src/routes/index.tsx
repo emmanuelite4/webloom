4b06ed733f907648f8f2582693ddf978edf439d1
Emmanuel <emmanuelite4@gmai.com>
2021-11-08 10:05:52 +0100

    Redirected path / to path /dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,14 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import { routes } from "./Routes";
+import { URL_DASHBOARD } from "../constants/navigations";
 
 export const Routes = () => {
   return (
     <Switch>
+      {/*<Redirect from="/" to={URL_DASHBOARD} />*/}
+      <Route path={"/"} exact>
+        <Redirect to={URL_DASHBOARD} />
+      </Route>
       {routes.map((route) => (
         <Route
           key={route.path}
